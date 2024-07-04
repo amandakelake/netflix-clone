d1e8ee30744fa2e0454e1af8ac4dce4200469177
LGC <luoguangcong@corp.netease.com>
2024-07-04 17:44:14 +0800

    feat: add button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        hello home page
+    <main className="min-h-screen m-4">
+        <h1 className="mb-1">hello home page</h1>
+        <Button>shadcn button</Button>
     </main>
   );
 }
