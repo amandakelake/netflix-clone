d099d4ef63e77ade68fbc050d5eb7d8ce3ac58dd
LGC <luoguangcong@corp.netease.com>
2024-07-07 20:02:41 +0800

    feat: log for test

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from 'next/navigation';
 
 export default async function Page() {
-	return redirect("/home/movie/shows");
+	return 'Home'
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { redirect } from 'next/navigation';
 
 export default async function Page() {
 	const session = await auth()
+	console.log('home page session', session)
 	if (!session) {
 		return redirect("/login");
 	} else {
